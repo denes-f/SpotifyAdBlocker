0c2db4b3806d3ccc52e29e3d26d9b669dcf565c9
Farago <Denes.Farago@avl.com>
2023-04-19 13:42:07 +0200

    Ad detection logic updated

--- spotify_ad_blocker.js
+++ spotify_ad_blocker.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Spotify AdBlocker
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Mute Spotify ads on the Spotify web player
 // @author       denes-f
 // @match        https://*.spotify.com/*
@@ -10,57 +10,76 @@
 // ==/UserScript==
 
 (function() {
-    console.log("Monkey start");
-    //setTimeout(muteSpotify, 5000);
-    const wordToCheck = 'reklám';
-    function checkTitleForWord() {
-        const pageTitle = document.title.toLowerCase();
-        if (pageTitle.includes(wordToCheck)) {
-            console.log(`The title "${pageTitle}" contains the word "${wordToCheck}"`);
-            // Do something else here, such as trigger another function or update the page content
-            muteSpotify();
-        }
-        else {
-            console.log(`The title "${pageTitle}" does not contain the word "${wordToCheck}"`);
-            unMuteSpotify();
-        }
-    }
-    // Create a new MutationObserver object
+    console.log("Spotify AdBlocker loaded");
+	const wordsToCheck = ['reklám'];
+	
+	// Create a new MutationObserver object to monitor changes to the site
     const observer = new MutationObserver(mutationsList => {
         // Check each mutation that has been observed
         for (const mutation of mutationsList) {
             // If the mutation is to the title element
             if (mutation.type === 'childList' && mutation.target.nodeName === 'TITLE') {
-                // Call the function to check the title for the word
-                checkTitleForWord();
+                // Call the function to check the title for an ad
+                checkTitleForAd(wordsToCheck);
             }
         }
     });
-
     // Start observing changes to the title element
     observer.observe(document.querySelector('head'), { subtree: true, childList: true });
 })();
 
+function checkTitleForAd(wordsToCheck) {
+	const pageTitle = document.title.toLowerCase();
+	for (const word of wordsToCheck) {
+		if (pageTitle.includes(word)) {
+			console.log(`The title "${pageTitle}" contains the word "${word}"`);
+			// Ad detected, muting Spotify
+			muteSpotify();
+			return;
+		}
+	}
+	console.log(`The title "${pageTitle}" does not contain any of the words "${wordsToCheck}"`);
+	// Music detected, unmuting Spotify
+	unMuteSpotify();
+}
+
 function muteSpotify() {
-    console.log("Monkey is muting Spotify");
+    console.log("Ad is detected");
     // Find the mute button element with the specified data-testid attribute
     const muteButton = document.querySelector('[data-testid="volume-bar-toggle-mute-button"]');
 
-    // Check if the aria-label attribute of the mute button is set to "Néma"
-    if (muteButton && muteButton.getAttribute('aria-label') === 'Néma') {
-        // Trigger the click event linked to the mute button
-        muteButton.click();
-    }
+	if (muteButton) {
+		// Check if the aria-label attribute of the mute button is set to "Néma"
+		if (muteButton.getAttribute('aria-label') === 'Néma') {
+			// Trigger the click event linked to the mute button
+			console.log("Muting Spotify");
+			muteButton.click();
+		}
+		else {
+			console.log("Spotify is already muted");
+		}
+	}
+	else {
+		console.log("Mute button cannot be found");
+	}
 }
 
 function unMuteSpotify() {
-    console.log("Monkey is unmuting Spotify");
+    console.log("Music is detected");
     // Find the mute button element with the specified data-testid attribute
     const muteButton = document.querySelector('[data-testid="volume-bar-toggle-mute-button"]');
 
-    // Check if the aria-label attribute of the mute button is not set to "Néma"
-    if (muteButton && muteButton.getAttribute('aria-label') !== 'Néma') {
-        // Trigger the click event linked to the mute button
-        muteButton.click();
-    }
+	if (muteButton) {
+		// Check if the aria-label attribute of the mute button is not set to "Néma"
+		if (muteButton.getAttribute('aria-label') !== 'Néma') {
+			// Trigger the click event linked to the unmute button
+			muteButton.click();
+		}
+		else {
+			console.log("Spotify is already unmuted");
+		}
+	}
+	else {
+		console.log("Unmute button cannot be found");
+	}
 }
